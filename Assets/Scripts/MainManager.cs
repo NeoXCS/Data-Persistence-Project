1d004c237dee3539c9739f377abe3f26095d7e99
NeoXCS <neomaster3x@gmail.com>
2024-06-06 13:34:24 -0400

    Revert "Merge branch 'main' of https://github.com/NeoXCS/Data-Persistence-Project"
    This reverts commit 49c12a4c5a7d703cdb9774c345e7e4b035800a8e, reversing
    changes made to 1ac4f56583e927f6ae3a589401bfd060c2939ff3.

--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -2,97 +2,75 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using System.IO;
-#if UNITY_EDITOR
-using UnityEditor;
-using UnityEditor.SearchService;
-#endif
+using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
+    public Brick BrickPrefab;
+    public int LineCount = 6;
+    public Rigidbody Ball;
 
-    public static MainManager Instance;
+    public Text ScoreText;
+    public GameObject GameOverText;
+    
+    private bool m_Started = false;
+    private int m_Points;
+    
+    private bool m_GameOver = false;
 
-    //Variables for current session
-    public string playerName;
-    public int currentScore;
-
-    //Variables for high score session
-    //Specify for other script compile errors
-    public int highScore;
-    public string highScoreName;
-
-    private void Awake()
+    
+    // Start is called before the first frame update
+    void Start()
     {
-        //If there are 2 MainManagers in scene, destroy this one
-        //Happens when other scene you move to tries to create its own MainManager
-        if(Instance != null)
+        const float step = 0.6f;
+        int perLine = Mathf.FloorToInt(4.0f / step);
+        
+        int[] pointCountArray = new [] {1,1,2,2,5,5};
+        for (int i = 0; i < LineCount; ++i)
         {
-            Destroy(gameObject);
-            return;
-        }
-
-        //But don't destroy MainManager if it's the only one
-        Instance = this;
-
-        DontDestroyOnLoad(gameObject);
-
-        //Do this, or the code wouldn't know a high score exists in MainManager
-        LoadHighScore();
-    }
-
-    //Load Game Scene
-    public void StartGame()
-    {
-        SceneManager.LoadScene(1);
-        //Make sure scenes are in correct order in File > Build Settings
-    }
-      
-      public void Exit()
-    {
-        #if UNITY_EDITOR
-            EditorApplication.ExitPlaymode();
-        #else
-            Application.Quit();
-        #endif
-    }
-
-    //Data Persistance across scenes
-    [System.Serializable]
-    public class SaveData
-    {
-        public int highScore;
-        public string highScoreName;
-    }
-
-    //This looks different from the tutorial as we need to capture two arguments
-    public void SaveHighScore(int currentScore, string playerName)
-    {
-        // First, create a new instance of the save data
-        SaveData data = new SaveData();
-
-        //Then specify what you want to store
-        data.highScore = currentScore;
-        data.highScoreName = playerName;
-
-        //Next, transform that instance to JSON with JsonUtility.ToJson
-        string json = JsonUtility.ToJson(data);
-
-        //Finally, use the special method File.WriteAllText to write a string to a file
-        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
-    }
-
-    public void LoadHighScore()
-    {
-        string path = Application.persistentDataPath + "/savefile.json";
-        if (File.Exists(path))
-        {
-            string json = File.ReadAllText(path);
-            SaveData data = JsonUtility.FromJson<SaveData>(json);
-
-            highScore = data.highScore;
-            highScoreName = data.highScoreName;
+            for (int x = 0; x < perLine; ++x)
+            {
+                Vector3 position = new Vector3(-1.5f + step * x, 2.5f + i * 0.3f, 0);
+                var brick = Instantiate(BrickPrefab, position, Quaternion.identity);
+                brick.PointValue = pointCountArray[i];
+                brick.onDestroyed.AddListener(AddPoint);
+            }
         }
     }
 
+    private void Update()
+    {
+        if (!m_Started)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                m_Started = true;
+                float randomDirection = Random.Range(-1.0f, 1.0f);
+                Vector3 forceDir = new Vector3(randomDirection, 1, 0);
+                forceDir.Normalize();
+
+                Ball.transform.SetParent(null);
+                Ball.AddForce(forceDir * 2.0f, ForceMode.VelocityChange);
+            }
+        }
+        else if (m_GameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            }
+        }
+    }
+
+    void AddPoint(int point)
+    {
+        m_Points += point;
+        ScoreText.text = $"Score : {m_Points}";
+    }
+
+    public void GameOver()
+    {
+        m_GameOver = true;
+        GameOverText.SetActive(true);
+    }
 }
